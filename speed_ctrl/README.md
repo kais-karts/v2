e69a79eef82534a5c1095feb9913e2b9ac4d63f0
AyanaAlemayehu <ayanakalemayehu@gmail.com>
2025-03-16 23:19:07 -0400

    WIP

--- speed_ctrl/README.md
+++ speed_ctrl/README.md
@@ -7,4 +7,6 @@ I will claim GPIO pins here
 
 ## Development Notes
 3/16/2025
+Im thinking Vh/Rh, VCC  connect to 5v. Vl/Rl, VSS connected to GND. Also all other pins defined in constants for now but just GPIOs.
+
 Speed controller breakouts arrived, watching [tutorial](https://www.youtube.com/watch?v=Zy50ZGSJLqM) introduces concern of potential amplifier needed to boost power of signal (10mW max power output of X9103). Should have some fail-fast protections in whatever I write (cause error whenever trying to be used when uninitialized, etc). Also will need to make a setup that is persistent across reboots -- no one wants to have to recalibrate cause the program power cycled (aka write to file before returning from function, read state from file)
--- speed_ctrl/__init__.py
+++ speed_ctrl/__init__.py
@@ -1,13 +1,89 @@
-def init_speed_control():
-    """
-    Initializes speed control system
-    """
+import RPi.GPIO as GPIO
+import time
 
+# this is ran upon import to force the initialization of the IC
+init_speed_control()
 
-def set_speed_multiplier(val: float):
+# CONSTANTS
+
+# pins
+CS_PIN  = 17
+INC_PIN = 27
+UD_PIN  = 22
+
+# other
+INITIALIZED = False
+
+# external functions
+def set_speed_multiplier(val: float) -> float:
     """
     Set the speed multiplier, e.g. how much of the pedal is applied to the actual motor speed controller.
-
+    Returns the attempted multiplier
+    Throws err if val out of bounds or not initalized
     Args:
         val (float): Value from 0.0 to 1.0, which multiplies the voltage of the pedal.
     """
+    if val < 0 or val > 1:
+        raise Exception(f"val {val} out of bounds, must be within [0:1]")
+    if INITIALIZED == False:
+        raise Exception("speed control IC not initialized")
+
+    val = round(val, 2)
+    reset_speed_control()
+    incs = int(val*100)
+    for i in range(incs):
+        increment()
+    
+    return val
+
+def reset_speed_control():
+    """
+    Decrements chip 99 times to guarentee starting at 0 tap of digital pot
+    """
+    for i in range(99):
+        decrement()
+
+def init_speed_control():
+    """
+    Initializes speed control ic
+    """
+    global INITIALIZED
+    GPIO.setwarnings(False)
+    GPIO.setmode(GPIO.BCM)
+
+    GPIO.setup(CS_PIN, GPIO.OUT)
+    GPIO.setup(INC_PIN, GPIO.OUT)
+    GPIO.setup(UD_PIN, GPIO.OUT)
+
+    GPIO.output(INC_PIN, GPIO.HIGH)
+    GPIO.output(CS_PIN, GPIO.LOW)
+    GPIO.output(UD_PIN, GPIO.LOW) # default decrement ig
+    INITIALIZED = False
+
+# util functions
+def increment():
+    """
+    increments by 1
+    """
+    GPIO.output(UD_PIN, GPIO.HIGH)
+    time.sleep(.001)
+    edgeTrigger()
+
+def decrement():
+    """
+    decrements by 1
+    """
+    GPIO.output(UD_PIN, GPIO.LOW)
+    time.sleep(.001)
+    edgeTrigger()
+
+def edgeTrigger():
+    """
+    triggers the INC line once
+    """
+    GPIO.output(INC_PIN, GPIO.HIGH)
+    time.sleep(.001)
+    GPIO.output(INC_PIN, GPIO.LOW)
+    time.sleep(.001)
+
+
