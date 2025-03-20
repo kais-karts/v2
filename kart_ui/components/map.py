cb2a93400bcef34a5ca97a22b838839ae2048466
Ty Todd <swagginty@gmail.com>
2025-03-20 00:17:00 -0400

    added mystery boxes on ui

--- kart_ui/components/map.py
+++ kart_ui/components/map.py
@@ -1,23 +1,29 @@
 from p5 import *
-from game_logic.constants import CHARACTERS
+from game_logic.constants import CHARACTERS, MYSTERY_BOXES
 MAP_WIDTH = 1024
 MAP_VERTICAL_OFFSET = 100
+KART_WIDTH = 100
+MYSTERY_BOX_WIDTH = 50
+
 global mouse_x, mouse_y
 class Map:
     def __init__(self, debugger):
         self.debugger = debugger
         self.map_image = loadImage("kart_ui/images/map.png")
-        self.kart_positions = {0: (0, 0), 1: (0, 0), 2: (0, 0), 3: (0, 0)}
+        self.kart_positions = {0: (0, 0), 1: (1080, 0), 2: (1080, 834), 3: (0, 834)}
         self.kart_images = {id: loadImage(f"kart_ui/images/heads/{CHARACTERS[id]}.png") for id in CHARACTERS}
         
         self.map_width = self.map_image.width()
         self.map_height = self.map_image.height()
         self.scale_size = MAP_WIDTH/self.map_width
-        self.origin_x = None
-        self.origin_y = None
+        self.origin_x = width / 4 - (self.map_width/2 * self.scale_size) 
+        self.origin_y = height / 2 - (MAP_VERTICAL_OFFSET * self.scale_size) - (self.map_height/2 * self.scale_size) 
+        
+        self.mystery_box_image = loadImage("kart_ui/images/mystery_box.webp")
         
     def draw(self):
         global width, height
+        # draw map
         img_width = self.map_width
         img_height = self.map_height
         scale_size = MAP_WIDTH/img_width
@@ -29,17 +35,44 @@ class Map:
         img_x = scaled_width/4
         img_y = scaled_height/2 - MAP_VERTICAL_OFFSET
         image(self.map_image, img_x-img_width/2, img_y-img_height/2)
+        self.debugger.show_image_outline(img_x, img_y, img_width, img_height, 'center')
         pop_matrix()
         
-        push_matrix()
-        translate(width/4, height/2 - MAP_VERTICAL_OFFSET)
+        # draw karts
         for kart_id, position in self.kart_positions.items():
-            x, y = position
             img = self.kart_images[kart_id]
-            image(img, x-img.width()/2, y-img.height()/2)
-        pop_matrix()
+            map_x, map_y = position
+            self.place_on_map(img, map_x, map_y, KART_WIDTH)
+        # draw mystery boxes
+        for box in MYSTERY_BOXES:
+            self.place_on_map(self.mystery_box_image, box[0], box[1], MYSTERY_BOX_WIDTH)
+        
     def update(self, kart_positions: dict):
         self.kart_positions = kart_positions
+    
+    def place_on_map(self, img, map_x, map_y, target_width):
+        image_scale_size = target_width/img.width()
+        push_matrix()
+        scale(image_scale_size)
+        
+        if map_x > self.map_width:
+            map_x = self.map_width
+        if map_y > self.map_height:
+            map_y = self.map_height
+        if map_x < 0:
+            map_x = 0
+        if map_y < 0:
+            map_y = 0
+            
+        x = self.origin_x + (map_x * self.scale_size) # get global x
+        y = self.origin_y + (map_y * self.scale_size) # get global y
+        
+        x = x / image_scale_size # get matrix x
+        y = y / image_scale_size # get matrix y
+        image(img, x-img.width()/2, y-img.height()/2)
+        self.debugger.show_image_outline(x, y, img.width(), img.height(), 'center')
+        pop_matrix()
+        
     
         
         
--- kart_ui/game_logic/constants.py
+++ kart_ui/game_logic/constants.py
@@ -8,3 +8,5 @@ BASE_MULTIPLIER = 0.5
 ITEMS = {'banana': (0, 3, False), 'bomb': (0, 10, False), 'redShroom': (1, 5, True), 'goldShroom': (1, 10, True), 'redShell': (0, 5, False), 'blueShell': (0, 7, False), 'lightning': (0.25, 5, False), 'bulletBill': (1, 10, True)}
 CHARACTERS = {0: 'mario', 1: 'bowser', 2: 'toad', 3: 'wario', 4: 'luigi', 5: 'waluigi'}
 MAP_METERS_PER_PIXEL = 2
+
+MYSTERY_BOXES = [(400, 647), (547, 789)]
