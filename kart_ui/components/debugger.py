af4f3708cf441037b552b3e92de5820396fb8b5b
Ty Todd <swagginty@gmail.com>
2025-04-01 22:19:55 -0400

    added api

--- kart_ui/components/debugger.py
+++ kart_ui/components/debugger.py
@@ -8,6 +8,7 @@ class Debugger:
         self.on = on
         self.shuffler = None
         self.map = None
+        self.warning = None
         
         
     def draw(self):
@@ -24,17 +25,26 @@ class Debugger:
             
             fill(255, 0, 0)
             circle(self.map.origin_x, self.map.origin_y, 50)
+            textSize(40)
+            textAlign(CENTER, BOTTOM)
+            text('bill get', width/4, 40)
+            text('banana hit', width/4, height - 40)
+            text('item use', 3*width/4, 40)
+            text('random get', 3*width/4, height - 40)
     
     def mouse_pressed(self, mouse_x, mouse_y):
         if self.on:
             self.show_luigi_at(mouse_x, mouse_y)    
         if self.shuffler is not None:
-            if mouse_x < width/2:
-                item = SHUFFLED_ITEMS[int(random_uniform(0, len(SHUFFLED_ITEMS)))]
-                # item = 'banana'
-                self.shuffler.shuffle(item)
-            else:
+            if 0 < mouse_x < width/2 and 0 < mouse_y < 200:
+                self.shuffler.shuffle('bulletBill')
+            elif 0 < mouse_x < width/2 and height - 200 < mouse_y < height:
+                self.warning.show('banana')
+            elif width/2 < mouse_x < width and 0 < mouse_y < 200:
                 self.shuffler.use_item()
+            elif width/2 < mouse_x < 3*width/4 and height - 200 < mouse_y < height:
+                item = SHUFFLED_ITEMS[int(random_uniform(0, len(SHUFFLED_ITEMS)))]
+                self.shuffler.shuffle(item)
     def show_image_outline(self, x, y, width, height, loc='center'):
         if self.on:
             stroke(255, 0, 0)  # Red border
@@ -54,11 +64,14 @@ class Debugger:
     def set_map(self, map):
         self.map = map
     
+    def set_warning(self, warning):
+        self.warning = warning
+    
     def show_luigi_at(self, mouse_x, mouse_y):
         map_x = (mouse_x - self.map.origin_x) / self.map.scale_size
         map_y = (mouse_y - self.map.origin_y) / self.map.scale_size
         new_positions = self.map.kart_positions.copy()
         new_positions[4] = (map_x, map_y)
-        print(map_x, map_y)
+        # print(map_x, map_y)
         self.map.update(new_positions)
         
--- kart_ui/components/warning.py
+++ kart_ui/components/warning.py
@@ -5,23 +5,72 @@ from p5.core.image import PImage
 from time import time
 global width, height
 
-FLASH_PERIOD = 0.1
+IMAGE_SIZE=900
+
+FLASH_PERIOD = 0.15
+
+class WarningState(Enum):
+    NO_WARNING = 0
+    IMAGE_ON = 1
+    IMAGE_OFF = 2
 
 class Warning:
-    def __init__(self):
-        self.show_warning = False
-        self.show_image = False
-        self.show_warning_start = 0
+    def __init__(self, debugger):
+        self.debugger = debugger
+        self.state = WarningState.NO_WARNING
+        self.warning_start = 0
         self.show_warning_duration = 0
-        self.show_image_start = 0 
+        self.flash_start = 0 
+        self.image = None
 
     def show(self, item: str):
-        self.show_warning = True
-        self.show_warning_start = time()
+        print("show warning")
+        self.warning_start = time()
         self.show_warning_duration = ITEMS[item][1]
-        self.show_image = True
-        self.show_image_start = time()
+        self.flash_start = time()
+        self.image = loadImage(f"kart_ui/images/items/{item}.png")
+        self.state = WarningState.IMAGE_ON
     
     def draw(self):
-        if self.show_warning:
-            text(self.warning, width/2, height/2)
+        global FLASH_PERIOD
+        # print(self.state)
+        if self.state == WarningState.IMAGE_ON:
+            background(255, 0, 0)
+            
+            self.place_item(self.image)
+            
+            if time() - self.warning_start > self.show_warning_duration:
+                self.state = WarningState.NO_WARNING
+            elif time() - self.flash_start > FLASH_PERIOD:
+                self.flash_start = time()
+                self.state = WarningState.IMAGE_OFF
+                
+        elif self.state == WarningState.IMAGE_OFF:
+            # background(255, 0, 0)
+            
+            if time() - self.warning_start > self.show_warning_duration:
+                self.state = WarningState.NO_WARNING
+            elif time() - self.flash_start > FLASH_PERIOD:
+                self.flash_start = time()
+                self.state = WarningState.IMAGE_ON
+    
+    def place_item(self, img):
+        global width, height
+        
+        img_width = img.width()
+        img_height = img.height()
+        
+        scale_size = min(IMAGE_SIZE / img_width, IMAGE_SIZE / img_height)
+        push_matrix()
+        scale(scale_size)
+        scaled_width = width / scale_size
+        scaled_height = height / scale_size
+
+        img_x = scaled_width/2
+        img_y = scaled_height/2
+        
+
+        image(img, img_x-img_width/2, img_y-img_height/2)
+        self.debugger.show_image_outline(img_x, img_y, img_width, img_height)
+        pop_matrix()
+            
